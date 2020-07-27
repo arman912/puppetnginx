0445d6838fa3cd4b5589b184ff51559c7d2f8c65
root <root@arman9121c.mylabserver.com>
2020-07-27 19:21:21 +0000

    add vhost config updated

--- environments/production/modules/nginx/manifests/vhosts.pp
+++ environments/production/modules/nginx/manifests/vhosts.pp
@@ -1,8 +1,18 @@
-# @summary A short summary of the purpose of this class
+# Generate a virtual hosts file for nginx. Requires node-specific Hiera data
 #
-# A description of what this class does
+# @summary Generate a virtual hosts file for nginx
 #
 # @example
 #   include nginx::vhosts
-class nginx::vhosts {
-}
+class nginx::vhosts (
+  $vhosts_dir = $nginx::params::vhosts_dir,
+) inherits nginx::params {
+  file { "${nginx::vhosts_name}.conf":
+    content => epp('nginx/vhosts.conf.epp'),
+    ensure  => $nginx::vhosts_ensure,
+    path    => "${vhosts_dir}/${nginx::vhosts_name}.conf",
+  }
+
+  file { "$nginx::vhosts_root":
+    ensure => 'directory',
+  }
